180db6c5873d1928b772571c044bf33deb7b883b
Brian McVeigh <bcmcveigh@gmail.com>
2022-11-19 12:39:46 -0500

    Install docker-compose.

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -33,12 +33,14 @@ echo "Installing Docker..."
 if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+# Install docker-compose.
+sudo apt-get install docker-compose -y
 
 # This will help make minukube work.
 sudo usermod -aG docker $USER && newgrp docker
