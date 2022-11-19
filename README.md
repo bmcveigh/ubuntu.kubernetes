b49d28a11069fec5f1442880e617ab6e564ced92
Brian McVeigh <bcmcveigh@gmail.com>
2022-11-19 09:44:29 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ubuntu Kubernetes
+
+Installs Kubernetes and Minikube on Ubuntu Desktop.
+
+## Installation instructions
+### Step 1: Clone and cd into the repository.
+``` bash
+https://github.com/bmcveigh/ubuntu.kubernetes.git
+cd ubuntu.kubernetes
+```
+
+### Step 2: Run the script.
+``` bash
+cat scripts/installer.sh | sh
+```
---
+++ scripts/installer.sh
@@ -0,0 +1,29 @@
+sudo apt-get update -y
+
+sudo apt install vim curl -y
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+echo "Installing Docker..."
+#sudo apt-get install docker.io -y
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+
+ sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# This will help make minukube work.
+sudo usermod -aG docker $USER && newgrp docker
+
