ce00f21ead412c142bd697812444934918c13683
Brian McVeigh <bcmcveigh@gmail.com>
2022-11-19 13:28:47 -0500

    Add a stack for drupal.

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -3,4 +3,9 @@
 # This also opens the dashboard in the browser.
 
 minikube start
+
+# TODO: dynamically add the services.
+cd stacks/drupal/kubernetes
+minikube kubectl -- apply -f cli-deployment.yaml,cli-service.yaml,db-deployment.yaml,db-service.yaml,web-deployment.yaml,web-service.yaml
+
 minikube dashboard
