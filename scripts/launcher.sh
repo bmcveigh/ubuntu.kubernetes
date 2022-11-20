3c376bf87cc5bb3f4721faa8f9701259b9e8a6bb
Brian McVeigh <bcmcveigh@gmail.com>
2022-11-20 11:34:46 -0500

    Print out the application URLs.

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -8,4 +8,7 @@ minikube start
 cd stacks/drupal/kubernetes
 minikube kubectl -- apply -f cli-deployment.yaml,cli-service.yaml,db-deployment.yaml,db-service.yaml,web-deployment.yaml,web-service.yaml
 
+echo "Below are the application endpoints that can be accessed in the browser..."
+minikube service web --url
+
 minikube dashboard
